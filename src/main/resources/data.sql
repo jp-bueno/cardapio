3d51ad03f1cded7582ffcc3a60f4aa154434854b
bueno <joaopedrobueno2910@gmail.com>
2023-05-18 08:18:29 -0300

    ajustes finais backend
    ajustes finais backend

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,5 @@
-INSERT INTO foods VALUES(1, 'Batata Frita', null, 20)
-INSERT INTO foods VALUES(2, 'Arroz', null, 15)
-INSERT INTO foods VALUES(3, 'Feijão', null, 10)
+--INSERT INTO foods VALUES(1, image, price, title)
+
+INSERT INTO Foods VALUES(1, 'https://conteudo.imguol.com.br/c/entretenimento/a0/2018/02/26/batata-frita-1519671488107_v2_900x506.jpg.webp', 20.00, 'Batata Frita')
+INSERT INTO Foods VALUES(2, 'https://static.itdg.com.br/images/1200-675/21fd76be3b29c3290859eda5220e0e32/323683-original.jpg', 15.60, 'Arroz')
+INSERT INTO Foods VALUES(3, 'https://static1.minhavida.com.br/articles/1e/b3/62/cf/minha-vida-feijao-article-1.jpg', 10.00, 'Feijao')
